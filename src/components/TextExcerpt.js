b760577c0da047da158063798837b2505af33152
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-27 14:25:47 -0800

    Each letter correct is colored green, each letter incorrect is colored red, each current word is underlined

--- src/components/TextExcerpt.js
+++ src/components/TextExcerpt.js
@@ -38,6 +38,7 @@ class TextExcerpt extends React.Component {
 		console.log("currentWord: " + currentWord);
 		console.log("currentLetter: " + currentLetter);
 		
+		//if current char is wrong, close tag and add <span className="wrong-char"></span>
 
 		if (typingFieldValue == (lastWord ? currentWord : currentWord + " ")) {
 			this.letterIndex = 0;
@@ -61,14 +62,44 @@ class TextExcerpt extends React.Component {
 		var correct = paragraph.slice(0, this.index).join(" ");
 		if (this.index < this.textArray.length)
 			correct += " ";
-		var rest = paragraph.slice(this.index).join(" ");
+		var rest = paragraph.slice(this.index+1).join(" ");
+
+
+		var word = paragraph[this.index];
+
+		var letterCorrect = "";
+		var letterIncorrect = "";
+		var letterRest = "";
+
+		var typingField = document.getElementById('typing-field');
+		var input = typingField ? typingField.value : "";
+
+		var incorrect = false;
+		for(var i=0; i<word.length; i++) {
+			if(input[i] == undefined) {
+				letterRest += word[i];
+			}
+			else if(!incorrect && word[i] == input[i]) {
+				letterCorrect += word[i];
+			} 
+			else {
+				letterIncorrect += word[i];
+				incorrect = true;
+			}
+		}
+		letterRest += " ";
 
 		return (
 			<div id="text-excerpt">
 					<div className="book">
 						<p id="paragraph">
-						<span className="right-char">{correct}</span>
-						{rest}
+							<span className="right-char">{correct}</span>
+							<span className="current-word">
+								<span className="right-char">{letterCorrect}</span>
+								<span className="wrong-char">{letterIncorrect}</span>
+								{letterRest}
+							</span>
+							{rest}
 						</p>
 					</div>
 			
