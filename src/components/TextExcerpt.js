98b898307c79bf4cf0c7e63b5c0aea2b6f936146
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-27 22:14:26 -0800

    Made so can't type in field until after button countdown finished, time elapsed stops when finished with last word

--- src/components/TextExcerpt.js
+++ src/components/TextExcerpt.js
@@ -5,30 +5,41 @@ class TextExcerpt extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			data: data.blurbs
+			data: data.blurbs,
+			timeElapsed: 0
 		}
-
+		this.startStopwatch = false;
 		this.pressedBackspace = false;
 		this.random = Math.floor(Math.random()*this.state.data.length);
 		this.text = this.state.data[this.random].text;
 		this.textArray = this.text.split(" ");
+		this.totalWords =  this.textArray.length;
 		this.index = 0;
-		
+		this.stopwatch;
 		this.handleChange = this.handleChange.bind(this);
-		this.onKeyDown = this.onKeyDown.bind(this);
+		this.displayTimeElapsed = this.displayTimeElapsed.bind(this);
+
 		console.log("this is pressedBackspace " + this.state.pressedBackspace);
 	}
 
-	onKeyDown(e) {
-		const keyCode = e.keyCode;
-		if(keyCode == 8) {
-			this.pressedBackspace = true;
-			this.setState({});
-		}
-		console.log("i'm in keydown and this is pressedBackspace " + this.pressedBackspace);
+	displayTimeElapsed() {
+		console.log("in timeElapsed function");
+		this.setState({
+			timeElapsed: this.state.timeElapsed+1
+		});
+
+		console.log("this is timeElapsed: " + this.state.timeElapsed);
 	}
 
 	handleChange() {
+		if (document.getElementById('btn-countdown'))
+		{
+			document.getElementById('typing-field').value="";
+			return;
+		}
+
+		if (!this.stopwatch)
+			this.stopwatch = setInterval(this.displayTimeElapsed, 1000);
 
 		var currentWord = this.textArray[this.index];
 		var typingFieldValue = document.getElementById('typing-field').value;
@@ -37,8 +48,6 @@ class TextExcerpt extends React.Component {
 
 		console.log("currentWord: " + currentWord);
 		console.log("currentLetter: " + currentLetter);
-		
-		//if current char is wrong, close tag and add <span className="wrong-char"></span>
 
 		if (typingFieldValue == (lastWord ? currentWord : currentWord + " ")) {
 			this.letterIndex = 0;
@@ -47,13 +56,10 @@ class TextExcerpt extends React.Component {
 
 			if (lastWord) {
 				// do something here. we finished typing everything
+				clearInterval(this.stopwatch);
 			}
 		}
 		this.setState({});
-	}
-
-	checkTyping() {
-		console.log("We here in checkTyping()");
 	}
 
 
@@ -62,8 +68,7 @@ class TextExcerpt extends React.Component {
 		var correct = paragraph.slice(0, this.index).join(" ");
 		if (this.index < this.textArray.length)
 			correct += " ";
-		var rest = paragraph.slice(this.index+1).join(" ");
-
+		var rest = this.index <= paragraph.length ? paragraph.slice(this.index+1).join(" ") : "";
 
 		var word = paragraph[this.index];
 
@@ -75,7 +80,7 @@ class TextExcerpt extends React.Component {
 		var input = typingField ? typingField.value : "";
 
 		var incorrect = false;
-		for(var i=0; i<word.length; i++) {
+		for(var i=0; word && i<word.length; i++) {
 			if(input[i] == undefined) {
 				letterRest += word[i];
 			}
@@ -102,8 +107,10 @@ class TextExcerpt extends React.Component {
 							{rest}
 						</p>
 					</div>
-			
-				<input id="typing-field" type="text" placeholder="Type here" autoFocus onKeyDown={this.onKeyDown} onChange={this.handleChange}/>
+				
+				<input id="typing-field" type="text" placeholder="Type here" autoFocus onChange={this.handleChange}/>
+				<p id="time-elapsed">Time Elapsed: {this.state.timeElapsed}</p>
+				<p>Total Words: {this.totalWords}</p>
 			</div>
 		)
 	}
