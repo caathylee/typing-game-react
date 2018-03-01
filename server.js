2bb42854db6930042b40212059516575e8ada597
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-28 17:36:06 -0800

    changed path to directory

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/'));
 
 app.get('*', (req, res) =>{
-  res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'src/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
