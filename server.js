2e210bf88bb13bc4e93737ca680f2e78a7a872a9
Cathy Lee <cathylee@cathys-mbp.lan>
2018-02-28 17:25:13 -0800

    Added app.get in server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/'));
+
+app.get('*', (req, res) =>{
+  res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
